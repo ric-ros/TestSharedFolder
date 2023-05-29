8d82af2b279f28b29befb36295c12f203efd49ed
Riccardo Rossetti <riccardo.rossetti@tgl.co>
2023-05-30 09:30:48 +1000

    Moved the entry point in the shared project

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,7 +9,7 @@ namespace TestSharedFolder.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<SharedApp>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
